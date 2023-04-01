b10600515a6e15db55400d12a418d0777d7c4b68
solahidris <emailsolah@gmail.com>
2023-04-02 07:06:40 +0800

    added questions and answer array, dynamically change question based on index count

--- src/App.js
+++ src/App.js
@@ -1,42 +1,48 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
-import { useEffect } from "react";
 
 function App() {
-  const [answerClicked, setAnswerClicked] = useState('');
+  const questionTitle = () => (
+    <div className="titleQuest">
+    <p>build a simple React quiz app where</p>
+    <p> 1- users can click on option buttons to answer questions</p>
+    <p> 1.5- store answer value</p>
+    <p> 2- next question rendered each time option is selected</p>
+    <p> 3- and at the end, the final score is displayed.</p>
+  </div>
+  );
 
-  const answerClickedHandler = (event) => {
-    setAnswerClicked(event.target.value);
+  const qnaArray = [
+    { question: "Question A - kalau ali dah tua, ali pakai apa?", answer: "Answer A" },
+    { question: "Question B", answer: "Answer B" },
+    { question: "Question C", answer: "Answer C" },
+    { question: "Question D", answer: "Answer D" },
+    { question: 'final score displayed here', answer: "total points from qna and remove button on the left here"}
+  ];
+
+  const [questionIndex, setQuestionIndex] = useState(0);
+
+  const questionIndexHandler = () => {
+    if (questionIndex+1 < qnaArray.length) {setQuestionIndex(questionIndex + 1)}
+    else {console.log("sini ganti dengan remove question and answer & display final score")}
   };
-  useEffect(() => {
-    console.log(answerClicked);
-  }, [answerClicked]);
 
-  // useEffect(() => {}, []);
+  const checkedHandler = () => {};
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div className="titleQuest">
-          <p>build a simple React quiz app where</p>
-          <p> 1- users can click on option buttons to answer questions</p>
-          <p> 1.5- store answer value</p>
-          <p> 2- next question rendered each time option is selected</p>
-          <p> 3- and at the end, the final score is displayed.</p>
-        </div>
+        {questionTitle()}
         <p>--------------------------------------------</p>
         <div className="qnaBox">
-          <p className="questions"> kalau ali dah tua, ali pakai apa? </p>
+          <p className="questions"> {qnaArray[questionIndex]["question"]} </p>
           <div className="answersContainer">
-            <label className="answers">
-              <input type="radio" name="inputA" value="A" checked={answerClicked === "A"} onChange={answerClickedHandler}></input> A) Tongkat Ali
+            <label>
+              <input type="radio" checked={checkedHandler} onChange={questionIndexHandler}></input>
+              {qnaArray[questionIndex]["answer"]}
             </label>
-            <label className="answers">
-              <input type="radio"></input> B) BMW
-            </label>
-            <p>answerclicked punya value: {answerClicked}</p>
           </div>
         </div>
       </header>
