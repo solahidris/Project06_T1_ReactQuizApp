cf89fd2539be9f61c739e566a63ba9cb46af213d
solahidris <emailsolah@gmail.com>
2023-05-02 15:30:22 +0800

    redo everything

--- src/App.js
+++ src/App.js
@@ -6,32 +6,13 @@ function App() {
   const questionTitle = () => (
     <div className="titleQuest">
     <p>build a simple React quiz app where</p>
-    <p> 1- users can click on option buttons to answer questions</p>
-    <p> 1.5- store answer value</p>
-    <p> 2- next question rendered each time option is selected</p>
-    <p> 3- and at the end, the final score is displayed.</p>
+    <p> 1- q1-q5 display with a1-a5</p>
+    <p> 1.5- store answer value when clicked</p>
+    <p> 2.0- change question after clicked</p>
+    <p> 2.5- new sum answer value once question changed</p>
+    <p> 3- and at the end, check if no more question, display answer sum</p>
   </div>
   );
-
-  // - kalau ali dah tua, ali pakai apa?"
-
-  const qnaArray = [
-    { question: "Question A", answer: "correctAnswer A1" },
-    { question: "Question B", answer: ["correctAnswer B1", "wrongAnswer B2"] },
-    { question: "Question C", answer: ["correctAnswer C1", "wrongAnswer C2", "wrongAnswer C3"] },
-    { question: "Question D", answer: ["correctAnswer D1", "wrongAnswer D2", "wrongAnswer D3", "wrongAnswer D4"] },
-  ];
-
-  const [questionIndex, setQuestionIndex] = useState(0);
-
-  const questionIndexHandler = () => {
-    if (questionIndex < qnaArray.length-1) {setQuestionIndex(questionIndex + 1)}
-    else {console.log("sini ganti dengan remove question and answer & display final score")}
-  };
-
-  const checkedHandler = () => {};
-
-  const answerList = qnaArray[questionIndex]["answer"];
 
   return (
     
@@ -40,29 +21,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         {questionTitle()}
         <p>--------------------------------------------</p>
-        
-        <p>{answerList}</p>
-
-        <div className="qnaBox">
-          <p className="questions"> {qnaArray[questionIndex]["question"]} </p>
-          <div className="answersContainer">
-
-            {/* Check if ANSWER is an array, if yes, map it. if no, display answer. */}
-            {Array.isArray(answerList) ? (
-              answerList.map((answer, index) => (
-                <label key={index}>
-                  <input type="radio" checked={checkedHandler} onChange={questionIndexHandler}/>
-                  {answer}
-                </label>
-              ))
-              ) : (
-                <label>
-                  <input type="radio" checked={checkedHandler} onChange={questionIndexHandler}/>
-                  {answerList}
-                </label>  
-              )}
-
-          </div>
+        <div>
+          <p>
+            Testing
+          </p>
         </div>
       </header>
     </div>
